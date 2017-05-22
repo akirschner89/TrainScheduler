bd5f1d33c39d19090f1e1939e0b01dc41895617f
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-21 20:34:16 -0500

    Updated JS

--- scheduleApp.js
+++ scheduleApp.js
@@ -21,30 +21,31 @@
     // Don't refresh the page!
     event.preventDefault();
 
-    var name = $("#employeeName").val().trim();
-    var role = $("#role").val().trim();
-    var date = $("#startDate").val().trim();
-    var rate = $("#monthlyRate").val().trim();
+    var name = $("#trainName").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrain = $("#firstTrain").val();
+    var frequency = $("#frequency").val().trim();
 
     // Log our inputs to see if the app is working
 
     console.log(name);
-    console.log(role);
-    console.log(date);
-    console.log(rate);
-    console.log(moment(date).format("X"));
+    console.log(destination);
+    console.log(firstTrain);
+    console.log(frequency);
+    console.log(moment(firstTrain).format("X"));
 
-    var monthToDate = moment(date).
+    // var monthToDate = moment(date).
 
 
     //Diplays the input information on Firebase
 
     database.ref().push({
 
-      rate: rate,
-      date: date,
-      role: role,
-      name: name
+      name: name,
+      destination: destination,
+      arrival: firstTrain,
+      frequency_min: frequency,
+      
     });
 
   });
@@ -55,26 +56,18 @@
 
       // Log everything that's coming out of snapshot
       console.log(childSnapshot.val().name);
-      console.log(childSnapshot.val().name);
-      console.log(childSnapshot.val().email);
-      console.log(childSnapshot.val().age);
-      console.log(childSnapshot.val().comment);
-      console.log(childSnapshot.val().joinDate);
+      console.log(childSnapshot.val().destination);
+      console.log(childSnapshot.val().arrival);
+      console.log(childSnapshot.val().frequency_min);
+      // console.log(childSnapshot.val().joinDate);
 
       // full list of items to the well
-      $(".table").append("<tr class='table-row'><td id='name'> " + childSnapshot.val().name +
-        " </td><td id='email'> " + childSnapshot.val().role +
-        " </td><td id='age'> " + childSnapshot.val().date +
-        " </td><td id='comment'> " + childSnapshot.val().rate + " </td></tr>");
+      $(".table").append("<tr class='table-row'><td class='name'> " + childSnapshot.val().name +
+        " </td><td class='destination'> " + childSnapshot.val().destination +
+        " </td><td class='frequency'> " + childSnapshot.val().frequency_min +
+        " </td><td class='arrival'> " + childSnapshot.val().arrival + " </td></tr>");
 
     // Handle the errors
     }, function(errorObject) {
       console.log("Errors handled: " + errorObject.code);
     });
-
-    // database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {    //   // Change the HTML to reflect
-    //   $("#name-display").html(snapshot.val().name);
-    //   $("#email-display").html(snapshot.val().email);
-    //   $("#age-display").html(snapshot.val().age);
-    //   $("#comment-display").html(snapshot.val().comment);
-    // });
